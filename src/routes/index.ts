7796f8be3f725765798ed824c9f5d96e11d347e3
xavierspacelix <juanakbar361@gmail.com>
2025-06-30 12:00:26 +0700

    Update greeting message in root route to include 'JUAN'

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello from TypeScript Express!");
+  res.send("Hello from TypeScript Express JUAN!");
 });
 
 export default router;
